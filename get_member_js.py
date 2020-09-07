d70b334a4f5225790d069478bae6f85c2b59eca1
Wang <henanwan@3c22fb3450bd.ant.amazon.com>
2020-09-07 10:45:16 +0800

    update member.js

---
+++ get_member_js.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#get_ipython().system('pip3 install beautifulsoup4')
+#get_ipython().system('pip3 install lxml')
+#get_ipython().system('pip3 install html5lib')
+
+
+# In[4]:
+
+
+import bs4
+import json
+
+
+# In[5]:
+
+
+exampleFile = open('js/members.txt')
+exampleSoup = bs4.BeautifulSoup(exampleFile.read(),'html5lib')
+# print(exampleSoup)
+elems = exampleSoup.select('.user-information')
+print(type(elems))
+print(len(elems))
+# print(elems[0])
+# print(elems[0].getText())
+
+members = []
+for i, elem in enumerate(elems):
+    # print(i, elem)
+    if i >= 2:
+        href = elem.select('a')
+        # print(href)
+        login = href[0]['href'][-7:]
+        name = href[0].string
+        member = {'phone': login, 'name': name}
+        members.append(member)
+
+# In[ ]:
+
+
+fout = open('js/member.js', 'w')
+
+fout.write('var member=')
+
+fout.write(json.dumps(members, indent=4))
+
+fout.close()
