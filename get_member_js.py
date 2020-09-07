80cf3cf6d99f48bf758b4864a81ba79831d8a8b1
Wang <henanwan@3c22fb3450bd.ant.amazon.com>
2020-09-07 14:48:46 +0800

    add photo

--- get_member_js.py
+++ get_member_js.py
@@ -14,33 +14,45 @@
 
 import bs4
 import json
+import pandas as pd
 
 
 # In[5]:
 
 
-exampleFile = open('js/members.txt')
-exampleSoup = bs4.BeautifulSoup(exampleFile.read(),'html5lib')
-# print(exampleSoup)
-elems = exampleSoup.select('.user-information')
-print(type(elems))
-print(len(elems))
-# print(elems[0])
-# print(elems[0].getText())
-
-members = []
-for i, elem in enumerate(elems):
-    # print(i, elem)
-    if i >= 2:
-        href = elem.select('a')
-        # print(href)
-        login = href[0]['href'][-7:]
-        name = href[0].string
-        member = {'phone': login, 'name': name}
-        members.append(member)
+# exampleFile = open('js/members.txt')
+# exampleSoup = bs4.BeautifulSoup(exampleFile.read(),'html5lib')
+# # print(exampleSoup)
+# elems = exampleSoup.select('.user-information')
+# print(type(elems))
+# print(len(elems))
+# # print(elems[0])
+# # print(elems[0].getText())
+#
+# members = []
+# for i, elem in enumerate(elems):
+#     # print(i, elem)
+#     if i >= 2:
+#         href = elem.select('a')
+#         # print(href)
+#         login = href[0]['href'][-7:]+'@'
+#         name = href[0].string
+#         member = {'phone': login, 'name': name}
+#         members.append(member)
 
 # In[ ]:
 
+persons = pd.read_excel('js/名单.xlsx')
+print(persons.shape)
+print(persons)
+
+members = []
+
+for idx, person in persons.iterrows():
+    name = person['Name']
+    login = person['Login']
+    member = {'phone': login, 'name': name}
+    members.append(member)
 
 fout = open('js/member.js', 'w')
 
